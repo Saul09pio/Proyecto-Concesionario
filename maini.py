4833a77a1cfcea7e1f7df629a6f53e38a24dbb31
Saul17 <martinezpardosaul@gmail.com>
2026-08-31 16:17:33 -0400

    ESTOS SON LOS NUEVOS DISCULPEN LA DEMORA

--- maini.py
+++ maini.py
@@ -1,48 +1,19 @@
 
-from Llantas import *
-from Base_data import *
-class Carros:
+class Taller:
 
-        def __init__(self, name, clase, model, price, type):
-            self.name = name
-            self.clase = clase
-            self.model = model
-            self.price = price
-            self.type = type
-            
-            
-        
-        
-        
-    
-def agregar():
-        input("Selecciona entre (LLantas, Motores, Pintura, Transmisores, Caja de cambio, etc):")
-    
-        if input("Selecciona entre (LLantas, Motores, Pintura, Transmisores, Caja de cambio, etc):") == "M":
-            pass
-        elif input("Selecciona entre (LLantas, Motores, Pintura, Transmisores, Caja de cambio, etc):") == "LL":
-            print(main_1())
-        elif input("Selecciona entre (LLantas, Motores, Pintura, Transmisores, Caja de cambio, etc):") == "R":
-            print("A agregado un motor remolque a su carrito")
-        else:
-            input("Select a piece: ")        
+    name_car = " "
+    def __init__(self,shop, inventroy):
+        self.shop = shop
+        self.inventroy = inventroy
 
+    def see_inventory(invetory):
+        pass
 
+    def change_piece():
+        pass
 
-def main():
-        cars = input("Put the name of your car: ")
-        print((f"You car is a {cars}"))
-        input("Deseas adquirir alguna pieza de carro (Y/N): ")
-    
+    def quit_piece():
+        pass
 
-    
-        while True:
-            if input("Deseas adquirir algunq pieza de carro (Y/N): ") == "Y":
-                print(agregar())
-            elif not input("Deseas adquirir algunq pieza de carro (Y/N): ") == "Y":
-                print("Respuesta invalida")
-                input("Deseas adquirir algunq pieza de carro (Y/N): ")
-
-
-if __name__ == "__main__":
-    print (main())
+    def go_shop(shop):
+        pass
